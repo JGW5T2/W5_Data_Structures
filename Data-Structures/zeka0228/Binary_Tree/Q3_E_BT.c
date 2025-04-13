485190b8181288fe57a450da29dda431ccda4513
zeka0228 <jhim0228@gmail.com>
2025-04-13 23:59:44 +0900

    update

--- Data-Structures/zeka0228/Binary_Tree/Q3_E_BT.c
+++ Data-Structures/zeka0228/Binary_Tree/Q3_E_BT.c
@@ -98,7 +98,7 @@ int main()
 
 //////////////////////////////////////////////////////////////////////////////////
 
-<<<<<<< HEAD
+
 int countOneChildNodes(BTNode *node){
     int count;
 
@@ -121,13 +121,7 @@ int countOneChildNodes(BTNode *node){
 
 }
 //checkcccc
-=======
-int countOneChildNodes(BTNode *node)
 
-{
-    /* add your code here */
-}
->>>>>>> 9bb02af4fab2601aef858ce32bbe7a00ca5df31c
 
 ///////////////////////////////////////////////////////////////////////////////////
 
